90437e3bb73d152a10ecedfa6f19f9b4f4a5fd74
Jack-Moon628 <bright_96_628@outlook.com>
2020-12-10 07:09:04 -0800

    implemented sign-in and sign-up with email

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 const HatsPage = () => (
   <div>
@@ -25,9 +25,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // //this.setState({ currentUser: user});
+      // createUserProfileDocument(user);
+      // // console.log(user);
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+            ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,14 +5,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCWwP7V-kwBMpMYcO5YM7FA9scXER8bDEM",
-  authDomain: "crwn-db-b8e2e.firebaseapp.com",
-  projectId: "crwn-db-b8e2e",
-  storageBucket: "crwn-db-b8e2e.appspot.com",
-  messagingSenderId: "789376179630",
-  appId: "1:789376179630:web:a5df0c3a5d2b26642c7c3f",
-  measurementId: "G-ERQ7Y7KK6X"
+  apiKey: "AIzaSyB9bCRZ-I7KfhimMRQjRC5g3OIXphvN85o",
+  authDomain: "crwn-db-7039c.firebaseapp.com",
+  projectId: "crwn-db-7039c",
+  storageBucket: "crwn-db-7039c.appspot.com",
+  messagingSenderId: "432091753238",
+  appId: "1:432091753238:web:bed41b78501aa669a37e37",
+  measurementId: "G-VFRTP6Y18Y"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
