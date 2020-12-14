b3a8c718697aa75832f3c2851ac8bd6109d167dd
Jack-Moon628 <bright_96_628@outlook.com>
2020-12-14 00:56:16 -0800

    redux completed

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-  render() {
-    const {collections} = this.state;
-    return (<div className='shop-page'>
-        {
-          collections.map(({id, ...ohterCollectionProps}) => (
-            <CollectionPreview key = {id} {...ohterCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+)
+
+
 
 export default ShopPage;
